2e4d652213afa2f8cb6b413194d6a86a140e9307
Jake Harding <jacob.s.harding@gmail.com>
2012-02-19 22:53:42 -0800

    Wrapping in closure.

--- stubsole.js
+++ stubsole.js
@@ -1,25 +1,27 @@
-if (typeof window.console === 'undefined') {
-    var noop = function() {};
-    window.console = {
-        log: noop,  
-        debug: noop,  
-        info: noop,  
-        warn: noop,  
-        error: noop,  
-        assert: noop,  
-        clear: noop,  
-        dir: noop,  
-        dirxml: noop,  
-        trace: noop,  
-        group: noop,  
-        groupCollapsed: noop,  
-        groupEnd: noop,  
-        time: noop,  
-        timeEnd: noop,  
-        profile: noop,  
-        profileEnd: noop,  
-        count: noop,  
-        exception: noop,  
-        table: noop 
-    };
-}
+(function() {
+    if (typeof window.console === 'undefined') {
+        var noop = function() {};
+        window.console = {
+            log: noop,  
+            debug: noop,  
+            info: noop,  
+            warn: noop,  
+            error: noop,  
+            assert: noop,  
+            clear: noop,  
+            dir: noop,  
+            dirxml: noop,  
+            trace: noop,  
+            group: noop,  
+            groupCollapsed: noop,  
+            groupEnd: noop,  
+            time: noop,  
+            timeEnd: noop,  
+            profile: noop,  
+            profileEnd: noop,  
+            count: noop,  
+            exception: noop,  
+            table: noop 
+        };
+    }
+})();
